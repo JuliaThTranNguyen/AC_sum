8a58f6e64c71eb4b2fae6edc6fe500d5dbd88acc
e1800930 <e1800930@edu.vamk.fi>
2021-10-09 20:20:19 +0300

    version 1

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+app = Flask(__name__, template_folder='templates')
+
+UPLOAD_FOLDER = './static/uploads'
+
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 24 * 1024 * 1024
